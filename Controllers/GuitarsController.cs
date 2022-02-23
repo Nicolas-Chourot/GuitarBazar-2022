1cb57ec47241acdfd137746faf0eeb2c65006b6b
Chourot Nicolas <Nicolas.Chourot@clg.qc.ca>
2022-02-23 14:28:01 -0500

    Add project files.

---
+++ Controllers/GuitarsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GuitarBazar.Models;
+
+namespace GuitarBazar.Controllers
+{
+    public class GuitarsController : Controller
+    {
+        Models.GuitarBazarDBEntities DB = new Models.GuitarBazarDBEntities();
+        // GET: Guitars
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.Conditions = SelectListItemConverter<Condition>.Convert(DB.Conditions.ToList());
+            ViewBag.GuitarTypes = SelectListItemConverter<GuitarType>.Convert(DB.GuitarTypes.ToList());
+            ViewBag.Sellers = SelectListItemConverter<Seller>.Convert(DB.Sellers.ToList(), false);
+            return View(new Guitar());
+        }
+        [HttpPost]
+        public ActionResult Create(Guitar guitar)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            ViewBag.Conditions = SelectListItemConverter<Condition>.Convert(DB.Conditions.ToList());
+            ViewBag.GuitarTypes = SelectListItemConverter<GuitarType>.Convert(DB.GuitarTypes.ToList());
+            ViewBag.Sellers = SelectListItemConverter<Seller>.Convert(DB.Sellers.ToList(), false);
+            return View(new Guitar());
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GuitarBazar.MvcApplication" Language="C#" %>
